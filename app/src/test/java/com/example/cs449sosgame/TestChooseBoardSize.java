967b64e3844a6b7e122b77f1e8eb96a902f481f9
Odai Athamneh <heyodai@gmail.com>
2022-03-19 20:32:53 -0500

    add board size unit tests

--- app/src/test/java/com/example/cs449sosgame/TestChooseBoardSize.java
+++ app/src/test/java/com/example/cs449sosgame/TestChooseBoardSize.java
@@ -1,4 +1,32 @@
 package com.example.cs449sosgame;
 
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
 public class TestChooseBoardSize {
+    protected ClassMatch match;
+
+    @Before
+    public void setUp() throws Exception {
+        match = new ClassMatch();
+    }
+
+    @Test
+    public void boardIsTooSmall() {
+        int boardSize = 2;
+        assertFalse(match.isBoardSizeValid(boardSize));
+    }
+
+    @Test
+    public void boardIsTooLarge() {
+        int boardSize = 9;
+        assertFalse(match.isBoardSizeValid(boardSize));
+    }
+
+    @Test
+    public void boardIsProperSize() {
+        int boardSize = 4;
+        assertTrue(match.isBoardSizeValid(boardSize));
+    }
 }
