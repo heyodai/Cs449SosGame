a5e78aa7247181f8bfa5f74522d50ccabb894264
Odai Athamneh <heyodai@gmail.com>
2022-03-20 10:06:32 -0500

    add game start tests

--- app/src/test/java/com/example/cs449sosgame/TestChooseGameMode.java
+++ app/src/test/java/com/example/cs449sosgame/TestChooseGameMode.java
@@ -1,4 +1,12 @@
 package com.example.cs449sosgame;
 
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
 public class TestChooseGameMode {
+    @Test
+    public void selectSimpleGameMode() {
+
+    }
 }
--- app/src/test/java/com/example/cs449sosgame/TestGameStart.java
+++ app/src/test/java/com/example/cs449sosgame/TestGameStart.java
@@ -1,4 +1,26 @@
 package com.example.cs449sosgame;
 
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
 public class TestGameStart {
+    protected ClassMatch match;
+
+    @Before
+    public void setUp() throws Exception {
+        match = new ClassMatch();
+    }
+
+    @Test
+    public void success() {
+        int boardSize = 4;
+        EnumMode gameMode = EnumMode.SIMPLE;
+        assertTrue(match.start(boardSize, gameMode));
+    }
+
+    @Test
+    public void failure() {
+        // to be written
+    }
 }
